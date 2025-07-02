759f14d92f96936e237256f1cf397140b30e796c
ivanDhanyjprihandana <IvanDhany17@gmail.com>
2025-07-02 20:36:23 +0700

    update feat tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Presensi - SDN 19 Kepahyang</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     @stack('styles')
